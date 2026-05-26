c202d2328a7a243b11d18feac5229c6cb202f3f2
kartiksoftwareengineer-blip <kartik.software.engineer@gmail.com>
2026-05-26 14:57:53 +0530

    add more learning and make a structure better of previous file by making folder for leacture 1 and we will continue like this

---
+++ leacture 1/fundamentals of python-1-2.33-__.py
@@ -0,0 +1,36 @@
+# Major types of Python modules:
+
+# 1. Built-in modules:
+# These come pre-installed with Python
+# Example: math, random, os, time, sys
+
+# 2. Standard library modules:
+# Official Python modules (already included but grouped as library)
+# Example: datetime, json, collections, re
+
+# 3. Third-party modules:
+# External modules installed using pip
+# Example: numpy, pandas, matplotlib, requests
+
+# 4. User-defined modules:
+# Modules created by programmer in their own project
+# Example: mymodule.py, calculator.py
+
+# 5. System/OS modules:
+# Used for interacting with system and files
+# Example: os, sys, subprocess
+
+#   6. Web modules:
+# Used for web development and APIs
+# Example: flask, django, requests
+
+# Arguments from command line
+
+import math
+
+print(math.sqrt(16))# it will give the square root of 16
+print(math.ceil(3.2))# it will give the upper value of 3.2
+print(math.floor(3.8))# it will give the lower value of 3.8
+print(math.pi)# it will give the value of pi
+print(math.e)# it will give the value of e
+print(math.pow(4,2))# it will give the square of 4
