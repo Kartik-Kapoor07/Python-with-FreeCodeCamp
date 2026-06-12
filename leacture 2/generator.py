75b394629284ed62f9d48bc4899d9e7474e739ef
kartiksoftwareengineer-blip <kartik.software.engineer@gmail.com>
2026-06-12 23:10:00 +0530

    #Update learning

---
+++ leacture 2/generator.py
@@ -0,0 +1,35 @@
+# A generator is a special type of function that produces a sequence of values lazily 
+# — one at a time, only when asked — instead of computing and storing everything in memory at once.
+# this is used for memory efficiency 
+# it just rember where it stop so that if need to run again generator know from where to run
+
+# yield run only once a time when called because they dont rember things
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+
+gen = my_generator()
+print(next(gen))  # 1
+print(next(gen))  # 2
+print(next(gen))  # 3
+
+
+
+def countdown(n):
+    while n > 0:
+        yield n      # pauses here, sends n to caller
+        n -= 1       # resumes here on next next()
+
+for val in countdown(5):
+    print(val)       # 5, 4, 3, 2, 1
+    
+# List comprehension — builds entire list in memory
+squares_list = [x**2 for x in range(1000000)]
+
+# Generator expression — computes one at a time
+squares_gen = (x**2 for x in range(1000000))
+
+print(squares_list)
+print(next(squares_gen))   # 0
+print(next(squares_gen))   # 1
